ea267a2912efeffa0671d725c43cb428fd3e7084
Kory Schneider <kory.schneider@gmail.com>
2016-08-26 10:48:18 -0700

    Make Mailbox no longer a renderable component
    Mailbox subcomponents can still be referred to like Mailbox.Controls or
    Mailbox.Attachment.

--- src/Mailbox.jsx
+++ src/Mailbox.jsx
@@ -1,45 +1,17 @@
 import React from 'react';
-import classNames from 'classnames';
 
-import MailboxControls from './MailboxControls';
-import MailboxMessages from './MailboxMessages';
-import MailboxAttachments from './MailboxAttachments';
-import MailboxReadMessage from './MailboxReadMessage';
-import MailboxReadInfo from './MailboxReadInfo';
+import Controls from './MailboxControls';
+import Messages from './MailboxMessages';
+import Attachments from './MailboxAttachments';
+import Attachment from './MailboxAttachment';
+import ReadMessage from './MailboxReadMessage';
+import ReadInfo from './MailboxReadInfo';
 
-const propTypes = {
-  style: React.PropTypes.oneOf(['default', 'primary', 'success', 'warning', 'danger']),
-  className: React.PropTypes.string,
-  children: React.PropTypes.node,
+export default {
+  Controls,
+  Messages,
+  Attachments,
+  Attachment,
+  ReadMessage,
+  ReadInfo,
 };
-
-const Mailbox = ({
-  style,
-  className,
-  children
-}) => {
-  const classes = {
-    box: true,
-    "box-primary": true,
-  };
-  if (style) {
-    classes[`box-${style}`] = true;
-    classes["box-primary"] = false;
-  }
-
-  return (
-    <div className={classNames(className, classes)}>
-      {children}
-    </div>
-  );
-};
-
-Mailbox.propTypes = propTypes;
-
-Mailbox.Controls = MailboxControls;
-Mailbox.Messages = MailboxMessages;
-Mailbox.Attachments = MailboxAttachments;
-Mailbox.ReadInfo = MailboxReadInfo;
-Mailbox.ReadMessage = MailboxReadMessage;
-
-export default Mailbox;
