5993335011d2b837de53b6d872f5eb9bd5e9d378
Kory Schneider <kory.schneider@gmail.com>
2016-08-30 16:19:48 -0700

    Merge SidebarUserPanel elements into one file
    Merged SidebarUserPanelImage and SidebarUserPanelImage into
    SidebarUserPanel. It is now similar to how BoxUserBlock is done. This is
    much less messy and removes a little bit of clutter.

--- test/SidebarUserPanelTest.jsx
+++ test/SidebarUserPanelTest.jsx
@@ -5,10 +5,12 @@ import test from 'tape';
 import SidebarUserPanel from '../src/SidebarUserPanel';
 
 test('<SidebarUserPanel>', t => {
-  const wrapper = shallow(<SidebarUserPanel>
-      <div>stuff</div>
-    </SidebarUserPanel>);
+  const wrapper = shallow(<SidebarUserPanel
+    image={<img src="#" />}
+    info={<div>stuff</div>}
+  />);
 
-  t.ok(wrapper.contains(<div>stuff</div>), 'renders children');
+  t.ok(wrapper.contains(<div className="pull-left image"><img src="#" /></div>), 'renders image');
+  t.ok(wrapper.contains(<div className="pull-left info"><div>stuff</div></div>), 'renders info');
   t.end();
 });
