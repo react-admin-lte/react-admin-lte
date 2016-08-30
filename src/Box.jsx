38b3650fdcde2103553e50d893143af5b1102da5
Kory Schneider <kory.schneider@gmail.com>
2016-08-30 11:15:23 -0700

    Make Box components more customizable
    All Box components now have prop className and utilize the classnames
    package.

--- src/Box.jsx
+++ src/Box.jsx
@@ -5,13 +5,13 @@ import FontAwesome from 'react-fontawesome';
 import BoxHeader from './BoxHeader';
 import BoxBody from './BoxBody';
 import BoxFooter from './BoxFooter';
-import BoxTitle from './BoxTitle';
-import BoxTools from './BoxTools';
 
 const propTypes = {
   style: React.PropTypes.oneOf(['default', 'primary', 'success', 'warning', 'danger']),
   type: React.PropTypes.oneOf(['widget', 'solid']),
   loading: React.PropTypes.bool,
+  className: React.PropTypes.string,
+  children: React.PropTypes.node,
 };
 
 const defaultProps = {
@@ -54,7 +54,5 @@ Box.defaultProps = defaultProps;
 Box.Header = BoxHeader;
 Box.Body = BoxBody;
 Box.Footer = BoxFooter;
-Box.Title = BoxTitle;
-Box.Tools = BoxTools;
 
 export default Box;
