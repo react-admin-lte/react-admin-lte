90df511f71f2ea98d21fd407b2885e8b5f8ff845
Kory Schneider <kory.schneider@gmail.com>
2016-08-30 11:28:11 -0700

    Update test

--- test/BoxToolsTest.jsx
+++ test/BoxToolsTest.jsx
@@ -10,7 +10,6 @@ test('<BoxTools>', t => {
   </BoxTools>);
 
   t.ok(wrapper.hasClass('box-tools'), 'has .box-tools class');
-  t.ok(wrapper.hasClass('pull-right'), 'has .pull-right class');
   t.ok(wrapper.contains(<div>stuff</div>), 'renders children');
   t.end();
 });
