f29888afa9cc81c875640dcfe2fa12282eb642f7
Kory Schneider <kory.schneider@gmail.com>
2016-09-12 09:02:54 -0700

    Accept additional classnames

--- src/LoginLogo.jsx
+++ src/LoginLogo.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import classNames from 'classnames';
 
 const propTypes = {
   children: React.PropTypes.node,
+  className: React.PropTypes.node,
 };
 
-const LoginLogo = ({ children }) => (
-  <div className="login-logo">
-    {children}
-  </div>
+const LoginLogo = ({ children }) => {
+  const classes = {
+    "login-logo": true,
+  };
+
+  return (
+    <div className={classNames(className, classes}>
+      {children}
+    </div>
+  );
 );
 
 LoginLogo.propTypes = propTypes;
