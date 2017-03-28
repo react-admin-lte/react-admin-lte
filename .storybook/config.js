e3a4b8429e2bf6904b576a9e74b9e24be8eaf74a
Thomas Gish <tgish@thomasgish.us>
2017-03-28 15:20:28 -0700

    Switch to Jest and storybook for testing

---
+++ .storybook/config.js
@@ -0,0 +1,13 @@
+import { configure } from '@kadira/storybook';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+import 'admin-lte/dist/css/AdminLTE.css';
+import 'admin-lte/dist/css/skins/skin-blue.css';
+
+const req = require.context('../src', true, /.stories.jsx$/);
+
+function loadStories() {
+  req.keys().forEach((filename) => req(filename));
+}
+
+configure(loadStories, module);
---
+++ src/Box.stories.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+import Box from './Box';
+
+const body = <Box.Body>The body of the box</Box.Body>;
+
+storiesOf('Box', module)
+  .addDecorator(story =>
+    <div
+      style={{
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: 20,
+        width: 400,
+      }}
+    >
+      {story()}
+    </div>
+  )
+  .add('default', () => (
+    <Box>
+      <Box.Header>
+        <Box.Title>Default Box Example</Box.Title>
+      </Box.Header>
+      {body}
+    </Box>
+  ))
+  .add('with style', () => (
+    <Box style="primary">
+      <Box.Header>
+        <Box.Title>Primary Box Example</Box.Title>
+      </Box.Header>
+      {body}
+    </Box>
+  ))
+  .add('solid with default style', () => (
+    <Box solid style="default">
+      <Box.Header>
+        <Box.Title>Solid Box Example</Box.Title>
+      </Box.Header>
+      {body}
+    </Box>
+  ))
+  .add('loading', () => (
+    <Box loading>
+      <Box.Header>
+        <Box.Title>Loading State</Box.Title>
+      </Box.Header>
+      {body}
+    </Box>
+  ))
+  .add('bordered header', () => (
+    <Box>
+      <Box.Header border>
+        <Box.Title>Bordered Box Example</Box.Title>
+      </Box.Header>
+      {body}
+    </Box>
+  ))
+  .add('footer', () => (
+    <Box>
+      <Box.Header>
+        <Box.Title>Footer</Box.Title>
+      </Box.Header>
+      {body}
+      <Box.Footer>The footer of the box</Box.Footer>
+    </Box>
+  ))
+  .add('body without padding', () => (
+    <Box>
+      <Box.Header>
+        <Box.Title>No padding</Box.Title>
+      </Box.Header>
+      <Box.Body noPadding>
+        The body of the box
+      </Box.Body>
+    </Box>
+  ));
