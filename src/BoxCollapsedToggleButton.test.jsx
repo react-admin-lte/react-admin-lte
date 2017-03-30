6ed0fa85fcc7fae029e33f591da52aa7e94141e4
Thomas Gish <tgish@thomasgish.us>
2017-03-30 15:01:15 -0700

    Pass context to enzyme directly

--- src/BoxCollapsedToggleButton.test.jsx
+++ src/BoxCollapsedToggleButton.test.jsx
@@ -1,33 +1,21 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import Box from './Box';
 import BoxCollapsedToggleButton from './BoxCollapsedToggleButton';
 
 test('Box notified when clicked', () => {
   let received = false;
 
-  class BoxFake extends React.Component {
-    static childContextTypes = Box.childContextTypes;
-
-    getChildContext() {
-      return {
+  const wrapper = mount(
+    <BoxCollapsedToggleButton />,
+    {
+      context: {
         $adminlte_box: {
           onCollapseToggle: () => { received = true; },
         },
-      };
-    }
+      },
+    },
+  );
 
-    render() {
-      return (
-        <div>
-          <BoxCollapsedToggleButton />
-        </div>
-      );
-    }
-  }
-
-  const wrapper = mount(<BoxFake />);
-
-  wrapper.find(BoxCollapsedToggleButton).simulate('click');
+  wrapper.simulate('click');
   expect(received).toEqual(true);
 });
