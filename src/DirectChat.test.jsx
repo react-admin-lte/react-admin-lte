a4718b967fd67d69aac0f2120d728a7cce3b28fa
Thomas Gish <tgish@thomasgish.us>
2017-03-31 13:37:48 -0700

    Test DirectChat receiving contact toggle

--- src/DirectChat.test.jsx
+++ src/DirectChat.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import DirectChat from './DirectChat';
 
 test('Passes message on form submit', () => {
@@ -18,4 +18,44 @@ test('Passes message on form submit', () => {
     preventDefault: () => {},
   });
   expect(received).toEqual(message);
+});
+
+class ToggleContacts extends React.Component {
+  constructor(props, context) {
+    super(props);
+    this.state = {
+      toggle: context.$adminlte_directchat.toggleContacts,
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  componentDidMount() {
+    this.state.toggle();
+  }
+
+  toggle() {
+    this.state.toggle();
+  }
+
+  static contextTypes = {
+    $adminlte_directchat: React.PropTypes.shape({
+      toggleContacts: React.PropTypes.func,
+    }),
+  };
+
+  render() {
+    return <a onClick={this.toggle} />;
+  }
+}
+
+test('Shown on toggle', () => {
+  const wrapper = mount(<DirectChat style="primary"><ToggleContacts /></DirectChat>);
+  expect(wrapper.state('contactsOpen')).toEqual(true);
+});
+
+test('Hidden on toggle', () => {
+  const wrapper = mount(<DirectChat style="primary"><ToggleContacts /></DirectChat>);
+  wrapper.find(ToggleContacts).find('a').simulate('click');
+  expect(wrapper.hasClass('direct-chat-contacts-open')).toEqual(false);
 });
