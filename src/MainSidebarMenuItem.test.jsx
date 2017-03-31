05b1fa1de627f9a3d0df9b16823f4222c712026c
Thomas Gish <tgish@thomasgish.us>
2017-03-31 11:28:59 -0700

    Test MainSidebarMenuItem click

---
+++ src/MainSidebarMenuItem.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import MainSidebarMenuItem from './MainSidebarMenuItem';
+
+test('calls onClick', () => {
+  let called = false;
+  const wrapper = shallow(<MainSidebarMenuItem
+    onClick={() => { called = true; }}
+  />);
+  wrapper.find('a').simulate('click');
+  expect(called).toEqual(true);
+});
