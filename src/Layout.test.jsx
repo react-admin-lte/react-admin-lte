311b6fd2510cc84d7de041b68806ba4feefa6c9e
Thomas Gish <tgish@thomasgish.us>
2017-03-31 09:58:47 -0700

    Test layout context functions directly

--- src/Layout.test.jsx
+++ src/Layout.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Layout from './Layout';
 import MainSidebar from './MainSidebar';
 
@@ -40,9 +40,9 @@ test('Layout removes body classes on unmount', () => {
     }
   }
 
-  const wrapper = mount(<Layout skin="blue" boxed><MainSidebarFake /></Layout>);
+  const wrapper = mount(<Layout skin="blue" boxed />);
 
-  expect(document.body.classList.contains('sidebar-collapse')).toEqual(true);
+  document.body.classList.add('sidebar-collapse');
 
   wrapper.unmount();
 
@@ -50,4 +50,64 @@ test('Layout removes body classes on unmount', () => {
   expect(document.body.classList.contains('sidebar-collapse')).toEqual(false);
   expect(document.body.classList.contains('skin-blue')).toEqual(false);
   expect(document.body.classList.contains('layout-boxed')).toEqual(false);
+});
+
+class SetCollapse extends React.Component {
+  constructor(props, context) {
+    super(props);
+    context.$adminlte_layout.setMainSidebarCollapsed(props.collapsed);
+  }
+
+  static propTypes = {
+    collapsed: React.PropTypes.bool.isRequired,
+  }
+
+  static contextTypes = {
+    $adminlte_layout: React.PropTypes.shape({
+      setMainSidebarCollapsed: React.PropTypes.func,
+    }),
+  };
+
+  render() {
+    return null;
+  }
+}
+
+test('Collapses sidebar on set', () => {
+  mount(<Layout skin="blue"><SetCollapse collapsed={true} /></Layout>);
+  expect(document.body.classList.contains('sidebar-collapse')).toEqual(true);
+});
+
+test('Expands sidebar on set', () => {
+  document.body.classList.add('sidebar-collapse');
+  mount(<Layout skin="blue"><SetCollapse collapsed={false} /></Layout>);
+  expect(document.body.classList.contains('sidebar-collapse')).toEqual(false);
+});
+
+class ToggleCollapse extends React.Component {
+  constructor(props, context) {
+    super(props);
+    context.$adminlte_layout.toggleMainSidebar();
+  }
+
+  static contextTypes = {
+    $adminlte_layout: React.PropTypes.shape({
+      toggleMainSidebar: React.PropTypes.func,
+    }),
+  }
+
+  render() {
+    return null;
+  }
+}
+
+test('Collapses sidebar on toggle', () => {
+  mount(<Layout skin="blue"><ToggleCollapse /></Layout>);
+  expect(document.body.classList.contains('sidebar-collapse')).toEqual(true);
+});
+
+test('Expands sidebar on toggle', () => {
+  document.body.classList.add('sidebar-collapse');
+  mount(<Layout skin="blue"><ToggleCollapse /></Layout>);
+  expect(document.body.classList.contains('sidebar-collapse')).toEqual(false);
 });
