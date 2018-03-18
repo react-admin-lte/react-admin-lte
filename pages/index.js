f5faf0e4f1482c3606d39b3b129607d33bd0661f
Thomas Gish <thomas.gish@mdemrsystems.com>
2018-03-17 23:27:40 -0700

    Add info boxes with progress to dashboard

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import MainHeader from 'reactjs-admin-lte/MainHeader';
 import Theme from 'reactjs-admin-lte/Theme';
 import Content from 'reactjs-admin-lte/Content';
 import InfoBox from 'reactjs-admin-lte/InfoBox';
+import InfoBoxProgressBar from 'reactjs-admin-lte/InfoBoxProgressBar';
+import Box from 'reactjs-admin-lte/Box';
+import BoxHeader from 'reactjs-admin-lte/BoxHeader';
+import BoxTitle from 'reactjs-admin-lte/BoxTitle';
 import Breadcrumb from 'react-bootstrap/lib/Breadcrumb';
 import BreadcrumbItem from 'react-bootstrap/lib/BreadcrumbItem';
 import Col from 'react-bootstrap/lib/Col';
@@ -65,6 +69,74 @@ const Home = () => {
               text="NEW MEMBERS"
               number="2,000"
             />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12}>
+            <Box>
+              <BoxHeader border>
+                <BoxTitle>Monthly Recap Report</BoxTitle>
+              </BoxHeader>
+            </Box>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={8}>
+            <Box style="success">
+              <BoxHeader border>
+                <BoxTitle>Visitors Report</BoxTitle>
+              </BoxHeader>
+            </Box>
+          </Col>
+          <Col md={4}>
+            <InfoBox
+              backgroundColor="yellow"
+              backgroundPlacement="box"
+              icon={<i className="ion ion-ios-pricetag-outline" />}
+              text="INVENTORY"
+              number="5,200"
+            >
+             <InfoBoxProgressBar
+               now={50}
+               description="50% Increase in 30 Days"
+             />
+            </InfoBox>
+            <InfoBox
+              backgroundColor="green"
+              backgroundPlacement="box"
+              icon={<i className="ion ion-ios-heart-outline" />}
+              text="MENTIONS"
+              number="92,050"
+            >
+              <InfoBoxProgressBar
+                now={20}
+                description="20% Increase in 30 Days"
+              />
+            </InfoBox>
+            <InfoBox
+              backgroundColor="red"
+              backgroundPlacement="box"
+              icon={<i className="ion ion-ios-cloud-download-outline" />}
+              text="DOWNLOADS"
+              number="114,381"
+            >
+              <InfoBoxProgressBar
+                now={70}
+                description="70% Increase in 30 Days"
+              />
+            </InfoBox>
+            <InfoBox
+              backgroundColor="aqua"
+              backgroundPlacement="box"
+              icon={<i className="ion ion-ios-chatbubble-outline" />}
+              text="DIRECT MESSAGES"
+              number="163,921"
+            >
+              <InfoBoxProgressBar
+                now={40}
+                description="40% Increase in 30 Days"
+              />
+            </InfoBox>
           </Col>
         </Row>
       </Content>
